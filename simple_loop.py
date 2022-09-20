5bbb170b4720dcdbddf2fbb3e2b1a6125fa8707f
Peter Deyi <peterdeyi@gmail.com>
2022-09-20 10:33:34 -0700

    intro to loops
    ISYS 350

---
+++ simple_loop.py
@@ -0,0 +1,16 @@
+print('I will display names')
+for name  in ['Peter','Paul','George'] :
+    print(name)
+
+print('I will display the HELLO 5x times')
+for x in range(5) :
+    print('Hello')
+
+
+print('Number\tSquare')
+print('--------------')
+
+for number in range(1,11):
+    square = number**2
+    print(number,'\tSqaure')
+
