7e562b523721972f0fe478ec44cd8752eb6b8bee
Peter Deyi <peterdeyi@gmail.com>
2023-02-25 21:14:35 -0800

    Add files via upload

---
+++ course_dict.py
@@ -0,0 +1,15 @@
+
+course_1 = {'CS: 101': ['#3004', 'Li', '8am' ]}
+
+course_2 = {'CS: 102': ['#4501', 'Naser', '9am']}
+
+course_3 = {'CS: 103': ['#6755', 'Sameer', '10am']}
+
+course_4 = {'ISYS: 350': ['#1111', 'Siker', '9:30am']}
+
+print(input("what is your course?"))
+
+if 'CS: 101' in course_1:
+    print(['#3004', 'Li', '8am'])
+
+
